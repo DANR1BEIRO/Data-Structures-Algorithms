62e94ebeac5c1530df43aca545afa9eb0f1c2850
DANR1BEIRO <danribeiro001@gmail.com>
2025-02-04 13:31:35 -0300

    sliding window

--- Arrays/sliding_window.py
+++ Arrays/sliding_window.py
@@ -1,32 +1,30 @@
+# maximum length substring with two occurrences
+
 class SlidingWindow:
     def maximumLenSubstring(self, string: str) -> int:
-        if not string:
-            return 0
-
-        left, right = 0, 0 # starting pointers
-        max_length = 1
-        counter = {} # store character counts
+        left, right = 0, 0
+        _max_length = 1
+        counter = {}
 
         counter[string[0]] = 1
 
         while right < len(string) -1:
             right += 1
 
-            if string[right] in counter:
+            if counter.get(string[right]):
                 counter[string[right]] += 1
             else:
                 counter[string[right]] = 1
 
-            while counter[string[right]] == 3:
+            while counter [string[right]] == 3:
                 counter[string[left]] -= 1
                 left += 1
 
-            max_length = max(max_length, right - left + 1)
+            _max_length = max(_max_length, right - left + 1)
 
-        return max_length
-
-s = SlidingWindow()
-print(s.maximumLenSubstring("aabbaaa")) # 4
-
-
-            
+        return _max_length
+    
+a = SlidingWindow()
+print(a.maximumLenSubstring("aabbaaa"))
+                
+     
